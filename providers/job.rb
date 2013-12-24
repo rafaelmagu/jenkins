7675c7e0556471ef62c8b6c67dea8de52770c0d4
Rafael Fonseca <rafael@bitsignal.com>
2013-12-24 17:08:27 +1300

    New cut of job checker

--- providers/job.rb
+++ providers/job.rb
@@ -28,12 +28,12 @@ end
 
 def action_update
   validate_job_config!
-  begin
+  if exists?
+    Chef::Log.debug("#{@new_resource} exists - updating")
     jenkins_cli "update-job #{@new_resource.job_name} < #{@new_resource.config}"
-    Chef::Log.debug("#{@new_resource} exists - updated")
-  rescue
+  else
+    Chef::Log.debug("#{@new_resource} does not exist - creating.")
     jenkins_cli "create-job #{@new_resource.job_name} < #{@new_resource.config}"
-    Chef::Log.debug("#{@new_resource} did not exist - created.")
   end
   new_resource.updated_by_last_action(true)
 end
@@ -61,5 +61,55 @@ private
 def validate_job_config!
   unless ::File.exist?(@new_resource.config)
     fail "'#{@new_resource.config}' does not exist or is not a valid Jenkins config file!"
+  end
+end
+
+def exists?
+  # ugly paste'n'hack to check if job exists without using http checks (this is auth-friendly)
+  url = node['jenkins']['server']['url']
+  home = node['jenkins']['node']['home']
+  username = node['jenkins']['cli']['username']
+  password = node['jenkins']['cli']['password']
+  password_file = node['jenkins']['cli']['password_file']
+  key_file = node['jenkins']['cli']['key_file']
+  jvm_options = node['jenkins']['cli']['jvm_options']
+
+  # recipes will chown to jenkins later if this doesn't already exist
+  directory 'home for jenkins-cli.jar' do
+    action :create
+    path node['jenkins']['node']['home']
+  end
+
+  cli_jar = ::File.join(home, 'jenkins-cli.jar')
+  remote_file cli_jar do
+    source "#{url}/jnlpJars/jenkins-cli.jar"
+    not_if { ::File.exists?(cli_jar) }
+  end
+
+  java_home = node['jenkins']['java_home'] || (node.attribute?('java') ? node['java']['java_home'] : nil)
+  if java_home.nil?
+    java = 'java'
+  else
+    java = '"' << ::File.join(java_home, 'bin', 'java') << '"'
+  end
+
+  java << " #{jvm_options}" if jvm_options
+
+  if key_file
+    command = "#{java} -jar #{cli_jar} -i #{key_file} -s #{url} get-job #{@new_resource.job_name}"
+  else
+    command = "#{java} -jar #{cli_jar} -s #{url} get-job #{@new_resource.job_name}"
+  end
+
+  command << " --username #{username}" if username
+  command << " --password #{password}" if password
+  command << " --password_file #{password_file}" if password_file
+
+  cmd = shell_out!(command, home)
+  Chef::Log.debug("#{@new_resource} get-job status: #{cmd.exitstatus}")
+  if cmd.exitstatus > 0
+    false
+  else
+    true
   end
 end
